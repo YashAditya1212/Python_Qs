4e73a0246f37bd146cc844310ffece04bf968e2d
Yash Aditya Mishra <146587626+YashAditya1212@users.noreply.github.com>
2026-06-14 20:51:59 +0530

    Add files via upload

---
+++ Check_if_directory_exits.py
@@ -0,0 +1,14 @@
+#opening a file with no permiosiions or does not exists:
+import os
+import pathlib
+
+# print(os.path.exits("path_whether_file_or_dir"))
+# print(os.path.isfile(path))  # will tell if file
+# print(os.path.isdir(path))  # will tell if file
+
+#in modern py we use pathlib module:
+path=paojefoief/efmeof/
+
+patyh.exists(path)
+path.is_file()
+path.is_dir()
